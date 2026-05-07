3dee0efbe6d37d46064e1508cc271e8aad5ef81e
Giga <gigamikasha@gemail.com>
2026-05-08 00:32:48 +0400

    create database in mysql workbench

--- src/main/java/com/solvd/musicstreamingservice/Main.java
+++ src/main/java/com/solvd/musicstreamingservice/Main.java
@@ -1,7 +1,23 @@
 package com.solvd.musicstreamingservice;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
 
+        String url = "jdbc:mysql://localhost:3306/music_streaming_service_db";
+        String user = "root";
+        String password = "898980";
+
+        try (Connection connection =
+                     DriverManager.getConnection(url, user, password)) {
+
+            System.out.println("Connected successfully!");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
