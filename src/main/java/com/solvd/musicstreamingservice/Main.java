8a8859ed31bd016601d6591cccd2a99c2aa5dbff
Giga <gigamikasha@gemail.com>
2026-05-08 19:51:11 +0400

    add music-streaming-service.mwb

--- src/main/java/com/solvd/musicstreamingservice/Main.java
+++ src/main/java/com/solvd/musicstreamingservice/Main.java
@@ -6,18 +6,6 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-
-        String url = "jdbc:mysql://localhost:3306/music_streaming_service_db";
-        String user = "root";
-        String password = "898980";
-
-        try (Connection connection =
-                     DriverManager.getConnection(url, user, password)) {
-
-            System.out.println("Connected successfully!");
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        
     }
 }
