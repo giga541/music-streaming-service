7d996e2ee8b26d905be2e69e3d4bdda586ced64c
Giga <gigamikasha@gemail.com>
2026-05-14 19:12:30 +0400

    update dml.sql

--- src/main/resources/dml.sql
+++ src/main/resources/dml.sql
@@ -2,17 +2,17 @@ INSERT INTO music_services(id, name, country)
 VALUES (1, 'RadioG', 'Georgia'),
        (2, 'TambiWave', 'Persia');
 
-INSERT INTO genres (id, name, description, music_service_id)
-VALUES (1, 'Soul', 'Soul music', 1),
-       (2, 'Rock', 'Guitar-driven music', 1);
-
-INSERT INTO albums (id, title, release_date, artist_id)
-VALUES (1, 'Pearls', '2011-01-24', 1),
-       (2, 'The Wall', '1979-11-30', 2);
+INSERT INTO genres (id, name, description)
+VALUES (1, 'Soul', 'Soul music'),
+       (2, 'Rock', 'Guitar-driven music');
 
 INSERT INTO artists (id, name, country, music_service_id)
 VALUES (1, 'Sade', 'UK', 1),
        (2, 'Pink Floyd', 'UK', 1);
+
+INSERT INTO albums (id, title, release_date, artist_id)
+VALUES (1, 'Pearls', '2011-01-24', 1),
+       (2, 'The Wall', '1979-11-30', 2);
 
 INSERT INTO songs (id, title, duration_seconds, release_date, album_id, genre_id)
 VALUES (1, 'Smooth Operator', 228, '2010-11-29', 1, 1),
@@ -39,10 +39,10 @@ VALUES (1, 'Morning Vibes', 1, 1),
        (3, 'Chill Evening', 1, 2),
        (4, 'Road Trip', 1, 3);
 
-INSERT INTO reviews (id, rating, comment, created_at, song_id)
-VALUES (1, 5, 'Absolute classic!', '2024-01-10 10:00:00', 1),
-       (2, 4, 'Love this song', '2024-01-15 14:30:00', 2);
-
 INSERT INTO stream_histories (id, played_at, listened_seconds, song_id)
 VALUES (1, '2024-03-01 08:00:00', 228, 1),
        (2, '2024-03-01 08:04:00', 285, 2);
+
+INSERT INTO reviews (id, rating, comment, created_at, song_id)
+VALUES (1, 5, 'Absolute classic!', '2024-01-10 10:00:00', 1),
+       (2, 4, 'Love this song', '2024-01-15 14:30:00', 2);
